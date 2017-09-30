61676d4c150a43695de881a3d25c89d552334419
Manuel Palmeira <mnp.manuel@gmail.com>
2017-09-30 01:44:25 +0100

    FAQ page added
    added faq page and functions to jquery to assist in animating the FAQ

--- js/chinadepot.js
+++ js/chinadepot.js
@@ -36,3 +36,78 @@
   });
 
 })(jQuery); // End of use strict
+
+var completes = document.querySelectorAll(".complete");
+var toggleButton = document.getElementById("toggleButton");
+
+function toggleComplete(d) {
+  var lastComplete = completes[completes.length - 1];
+  lastComplete.classList.toggle('complete');
+}
+
+toggleButton.onclick = toggleComplete;
+
+function HideContent(d) {
+document.getElementById(d).style.display = "none";
+}
+function ShowContent(d) {
+document.getElementById(d).style.display = "block";
+}
+function ReverseDisplay(d) {
+if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
+else { document.getElementById(d).style.display = "none"; }
+}
+
+function HideAllShowOne(d) {
+// Between the quotation marks, list the id values of each div.
+
+    var IDvaluesOfEachDiv = "displayText7 displayText8 displayText9 displayText10 displayText11 displayText12 displayText13 displayText displayText1 displayText2 displayText3 displayText4 displayText5 displayText6";
+
+    //-------------------------------------------------------------
+    IDvaluesOfEachDiv = IDvaluesOfEachDiv.replace(/[,\s"']/g," ");
+    IDvaluesOfEachDiv = IDvaluesOfEachDiv.replace(/^\s*/,"");
+    IDvaluesOfEachDiv = IDvaluesOfEachDiv.replace(/\s*$/,"");
+    IDvaluesOfEachDiv = IDvaluesOfEachDiv.replace(/  +/g," ");
+    var IDlist = IDvaluesOfEachDiv.split(" ");
+    for(var i=0; i<IDlist.length; i++) { HideContent(IDlist[i]); }
+    ShowContent(d);
+    if(document.getElementById(d).style.display == "block")  { HideContent(d); }
+}
+
+function showonlyonev2(thechosenone) {
+      var newboxes = document.getElementsByTagName("div");
+      for(var x=0; x<newboxes.length; x++) {
+            name = newboxes[x].getAttribute("class");
+            if (name == 'newboxes-2') {
+                  if (newboxes[x].id == thechosenone) {
+                        if (newboxes[x].style.display == 'block') {
+                              newboxes[x].style.display = 'none';
+                        }
+                        else {
+                              newboxes[x].style.display = 'block';
+                        }
+                  }else {
+                        newboxes[x].style.display = 'none';
+                  }
+            }
+      }
+}
+
+function showonlyonev3(thechosenone) {
+    var newboxes = document.getElementsByTagName("tr");
+    for(var x=0; x<newboxes.length; x++) {
+          name = newboxes[x].getAttribute("class");
+          if (name == 'newboxes-2') {
+                if (newboxes[x].id == thechosenone) {
+                      if (newboxes[x].style.display == 'block') {
+                            newboxes[x].style.display = 'none';
+                      }
+                      else {
+                            newboxes[x].style.display = 'block';
+                      }
+                }else {
+                      newboxes[x].style.display = 'none';
+                }
+          }
+    }
+}
